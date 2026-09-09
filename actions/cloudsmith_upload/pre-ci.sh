1757e3b46d63d0fb5f81c48b25f0ad2d23a40847
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2026-09-08 22:24:05 -0400

    feat(cloudsmith_upload): add Alpine APK support

--- actions/cloudsmith_upload/pre-ci.sh
+++ actions/cloudsmith_upload/pre-ci.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 package_path=$(jq -r '.package_path' <<< "${WITH_PARAMS}")
 mkdir -p "${package_path}"
 touch \
+  "${package_path}/helloworld_1.2.3_alpine3.24_x86_64.apk" \
   "${package_path}/helloworld_1.2.3-1+ubuntu22.04_amd64.deb" \
   "${package_path}/HelloWorld-1.2.3-1.fc44.x86_64.rpm" \
   "${package_path}/HelloWorld-1.2.3-1.suse.lp156.aarch64.rpm"
